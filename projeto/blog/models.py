255326cb7ae494a182c51e43480d3a2ee150f1d1
Guilherme <gui.msr.98@gmail.com>
2017-07-24 20:45:56 -0300

    Trabalho de Comentários

--- projeto/blog/models.py
+++ projeto/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    data_criacao = models.DateTimeField(default=timezone.now)
+    data_publicacao = models.DateTimeField(null = True, blank = True)
+    autor = models.ForeignKey("auth.User")
+
+    def __str__(self):
+        return str(self.titulo.encode("utf-8"))
+
+
+class Comentario(models.Model):
+    texto = models.TextField()
+    data_publicacao = models.DateTimeField(default=timezone.now())
+    autor = models.ForeignKey('auth.User')
+    post = models.ForeignKey('Post')
--- projeto/blog/views.py
+++ projeto/blog/views.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.shortcuts import render
+from .models import Post
+from .models import Comentario
+from django.utils import timezone
 
 # Create your views here.
+def post_list(request):
+    posts_publicados = Post.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
+    return render(request, "post_list.html", {'posts': posts_publicados})
+
+def coment_list(request):
+    comentarios_publicados = Comentario.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
+    return render(request, "comment_list", {'comentarios': comentarios_publicados})
